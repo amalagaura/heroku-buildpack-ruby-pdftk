19c54eab808d6127ab5d0b1e96ea7098f38cb3e2
Ankur Sethi <ankur.sethi@ntrepidcorp.com>
2013-01-03 19:45:54 -0500

    Add another set of sources

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -1,6 +1,6 @@
 class Pdftk < BaseCustom
   S3_PREFIX='http://s3.amazonaws.com/heroku_binaries'
-  NAME_SOURCES={ pdftk: "#{S3_PREFIX}/pdftk.tar.gz", poppler:  "#{S3_PREFIX}/heroku-poppler.tgz"}
+  NAME_SOURCES={ pdftk: 'pdftk.tar.gz', poppler:  'heroku-poppler.tgz', pdf2text: 'pdf2text.tgz'}
   def path
     "#{build_path}/vendor"
   end
@@ -8,7 +8,7 @@ class Pdftk < BaseCustom
     NAME_SOURCES.each do |name,source_url|
       write_stdout "copying #{name}"
       #download the source and extract
-      %x{ mkdir -p #{path} && curl --silent #{source_url} -o - | tar -xz -C #{path} -f - }
+      %x{ mkdir -p #{path} && curl --silent "#{S3_PREFIX}/#{source_url}" -o - | tar -xz -C #{path} -f - }
       write_stdout "complete extracting #{name}"
     end
   end
